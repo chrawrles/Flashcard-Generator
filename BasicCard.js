0c6c13302067584a0e2edf7aac45554a8a975d8e
chrawrles <chrawrles@gmail.com>
2017-08-30 22:46:27 -0700

    Delete BasicCard.js

--- BasicCard.js
+++
@@ -1,9 +0,0 @@
-// Constructor function for the basic card
-function BasicCard(front, back) {
-        this.front = front;
-        this.back = back;
-
-};
-
-// Exports the basic card constructor
-module.exports = BasicCard;
